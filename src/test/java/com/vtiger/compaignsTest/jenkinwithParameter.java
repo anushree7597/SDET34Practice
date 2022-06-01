e0b49b29be222b07a97d93ec0d03f7bbbaf46ff7
anushree7597 <106369227+anushree7597@users.noreply.github.com>
2022-06-01 10:06:53 +0530

    Update jenkinwithParameter.java

--- src/test/java/com/vtiger/compaignsTest/jenkinwithParameter.java
+++ src/test/java/com/vtiger/compaignsTest/jenkinwithParameter.java
@@ -12,7 +12,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class jenkinwithParameter {
-	
+	//changes from b2
 	public void testStep1Test()
 	{
 		String browser=System.getProperty("browser");
